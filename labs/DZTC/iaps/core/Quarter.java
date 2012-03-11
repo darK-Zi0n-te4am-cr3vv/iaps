95a56b1064bb90364c49bb6d77eb0e8e3fe1caf4
C.c <fishbone1319@gmail.com>
2012-03-12 00:32:25 +0400

    minor changes

--- labs/DZTC/iaps/core/Quarter.java
+++ labs/DZTC/iaps/core/Quarter.java
@@ -4,31 +4,8 @@ import java.lang.*;
 
 public enum Quarter
 {
-	I(1), 
-	II(2), 
-	III(3), 
-	IV(4);
-	
-	private Quarter(int aint) { _aint = aint; }
-	
-	private int _aint;
-	
-	public int toInt()
-	{
-		return _aint;
-	}
-	
-	public static Quarter fromInt(int i) 
-		throws IllegalArgumentException
-	{
-		switch (i)
-		{
-			case 1 : return I;
-			case 2 : return II;
-			case 3 : return III;
-			case 4 : return IV;
-			
-			default : throw new IllegalArgumentException("invalid quarter");
-		}
-	}
+	I, 
+	II, 
+	III, 
+	IV
 }
